f872f47c8a33e9fb740e3f346264d73c704cf7fd
Lucas <pv.lucassilva@gmail.com>
2024-07-04 10:16:16 -0300

    first commit

---
+++ src/main/java/br/com/hotel/hotel/HotelcodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.hotel.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelcodeApplication.class, args);
+	}
+
+}
